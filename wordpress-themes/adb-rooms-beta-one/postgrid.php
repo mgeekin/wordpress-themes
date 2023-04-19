e896cdd47c62bcf4eb69a790f0fb28be699bfa01
prateekrajgautam <prateekrajgautam@gmail.com>
2023-04-19 17:50:48 +0545

    update

--- wordpress-themes/adb-rooms-beta-one/postgrid.php
+++ wordpress-themes/adb-rooms-beta-one/postgrid.php
@@ -145,3 +145,32 @@
         </div>
     </div>
 </div>
+
+<script>
+imageUrl = [
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg/1024px-Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg",
+    "https://en.wikipedia.org/wiki/File:Mt._Kilimanjaro_12.2006.JPG",
+    "https://en.wikipedia.org/wiki/File:Puncakjaya.jpg",
+    "https://en.wikipedia.org/wiki/File:080103_hakkai_fuji.jpg",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.a20Rx0lnOyPXgCS3MjbpJgHaEK%26pid%3DApi&f=1&ipt=f675c2b238bf953d0b87ed8cf8d628efa355fe67ffdbf7130ea8206767f1351b&ipo=images",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.TOBF1OMA2asBS2WFRF_qdAHaEK%26pid%3DApi&f=1&ipt=2871770a776964b2ff677ae2daa26de36ffc81a3eeb8ed2214fa0d1735ee5fca&ipo=images"
+]
+
+imageUrl.forEach((imgurl, count) => {
+    var postId = `post${count}`;
+    append(postgrid, gen('div', postId, , `postgrid-item`), 'before');
+    append(postId, gen('div', '', gen(img, "", "", "", {
+        "loading": "eager",
+        "src": imgurl,
+        "alt",
+        "image"
+    }), `postgrid-item-image`));
+    var contentID = "content" + count;
+    append(postId, gen('div', contentID, gen(h1, , "Heading", "postgrid-item-title"), `postgrid-item-content`));
+    append(contentID, gen(p, ,
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, voluptate, voluptatem, quae, quia voluptates quibusdam quos quas nesciunt quidem.",
+        "postgrid-item-excerpt"));
+
+
+});
+</script>
